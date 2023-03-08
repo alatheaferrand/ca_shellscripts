1178cba88f009940865079ac1299326dac33b1cb
alatheaferrand <83938198+alatheaferrand@users.noreply.github.com>
2023-03-07 22:15:19 -1000

    Arrays màj

--- arrays.sh
+++ arrays.sh
@@ -6,5 +6,8 @@ STRINGS=(hello world)
 NumberOfNames=${#NAMES[@]}
 second_name=${NAMES[1]}
 
+echo ${NUMBERS[@]}
+echo ${STRINGS[@]}
+echo "Names: ${NAMES[@]}"
 echo "Number of Names: $NumberOfNames"
 echo "The second name: $second_name"
